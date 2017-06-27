da78ca32d4bc93404b5ec30fe6ba29215fc2a574
ZenabOwaid <eng-zena@hotmail.com>
2017-06-27 19:11:00 -0400

    Done

---
+++ Maths/ScoreKeeper.h
@@ -0,0 +1,13 @@
+//
+//  ScoreKeeper.h
+//  Maths
+//
+//  Created by Zenab Owaid on 6/27/17.
+//  Copyright © 2017 Zenab Owaid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScoreKeeper : NSObject
+
+@end
---
+++ Maths/ScoreKeeper.m
@@ -0,0 +1,13 @@
+//
+//  ScoreKeeper.m
+//  Maths
+//
+//  Created by Zenab Owaid on 6/27/17.
+//  Copyright © 2017 Zenab Owaid. All rights reserved.
+//
+
+#import "ScoreKeeper.h"
+
+@implementation ScoreKeeper
+
+@end
