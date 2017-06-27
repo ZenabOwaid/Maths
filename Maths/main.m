b937d92c22568f0dce1597ab3e2642c2692dccd6
ZenabOwaid <eng-zena@hotmail.com>
2017-06-27 11:35:49 -0400

    initial commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Zenab Owaid on 6/27/17.
+//  Copyright © 2017 Zenab Owaid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
